6c2e064b8ecfed11910eda2585ea61a81b4ca059
Remko Weijers <30292426+weij3721@users.noreply.github.com>
2018-10-02 00:52:06 +0200

    first attempt
    placed first ship randomly
    and upload position file to S3

---
+++ shouldRenderRandomBoard_test.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Oct  1 23:11:35 2018
+
+@author: weij3721
+"""
+
+#Given that board size = 10
+#And number of ships = 1
+#and length = 2
+#When the board is generated
+#Then the ship is valid on the board
+
