a3c8649094b331995832245e210caefc49dbe007
Remko Weijers <30292426+weij3721@users.noreply.github.com>
2018-10-01 21:43:23 +0200

    Clean up done
    commented test code, will remove later when functions are separated,
    added tests to cover validation

--- Battleship_HitMiss.py
+++ Battleship_HitMiss.py
@@ -1,6 +1,7 @@
-
+import boto3
+import string
 from chalice import Chalice
-app = Chalice(app_name='mychaliceapp')
+app = Chalice(app_name='mychaliceapp2')
 
 @app.route('/battleship/fired/{positionFiredAt}')
 
@@ -8,15 +9,49 @@ app = Chalice(app_name='mychaliceapp')
 def HitOrMiss(positionFiredAt):
     # to do validate if this is a valid one, given the existing board
     #never trust the input
+    s3 = boto3.resource('s3')
+    obj = s3.Object("battleship-ship-positions", "battleship_boardsize_game1.txt")
+    boardsize = int(obj.get()['Body'].read().decode('utf-8'))
+    if boardsize > 26:
+        return {"boardsize invalid"}
+    
+    #to do: error handling for Not A Number
+    #print(boardsize)
+    
+    #split input into two, first two characters only
+    #positionFiredAt="p8"
+    #print (len(positionFiredAt))
+    if 0 < len(positionFiredAt) < 4:
+        FirstCharacter = positionFiredAt[0]
+        FirstCharacter = FirstCharacter.upper()
+        SecondCharacter = int(positionFiredAt[1:2])
+     #   print (FirstCharacter, SecondCharacter)
         
+        #compare
+        if not (FirstCharacter <= string.ascii_uppercase[boardsize] \
+                and (SecondCharacter <= boardsize or SecondCharacter < 27)):
+                #print ("Invalid input for target")
+                return{"Invalid input for target"}
+       # else:
+                #print("input valid")
+        #        return("Invalid input for target")
+        
+    else:
+        #print ("not valid")
+        return{"Invalid input for target, too long"}
     # get the list of targetareas
-    positionOfShips = ("a1","a2","e5")
+    #positionOfShips = ("a1","a2","e6")
+   
+
+    obj = s3.Object("battleship-ship-positions", "battleship_positions_game1.txt")
+    positionOfShips = obj.get()['Body'].read().decode('utf-8')
+    print (positionOfShips)
             
     #evaluate
     if positionFiredAt in positionOfShips:
-        return{"Hit"}
+        return{"Hit", positionFiredAt}
     elif positionFiredAt != positionOfShips:
-        return{"Miss"}
+        return{"Miss", positionFiredAt}
     else:
         return{"Error"}
     
