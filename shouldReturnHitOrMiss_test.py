69a9edc00fc55f0c0c0eb2a4cae77ba40bc097ea
Remko Weijers <30292426+weij3721@users.noreply.github.com>
2018-10-01 21:58:46 +0200

    Corrections done to fix incorrect merge to master

--- shouldReturnHitOrMiss_test.py
+++ shouldReturnHitOrMiss_test.py
@@ -7,12 +7,17 @@ from Battleship_HitMiss import HitOrMiss
 #Then it's reported as hit
 
 def test_Hit():
-    
-    assert HitOrMiss("e6") == {"Hit", 'e6'}
+   assert HitOrMiss("e6") == {"Hit", 'e6'}
 
 #Given that no ship is present at location d4
 #When a missile hits target d4
 #Then it's reported as miss
 def test_Miss():
-    
     assert HitOrMiss("d4") == {"Miss", 'd4'}
+
+def test_input_too_long():
+    assert HitOrMiss("d333") == {"Invalid input for target, too long"}
+    
+def test_input_outside_of_maxboardsize():
+    assert HitOrMiss ("p27") == {"Invalid input for target"}
+    
